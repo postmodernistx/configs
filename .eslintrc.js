0f6b25e5630dbd9254c31a5381335cf9e25e623c
Jenni <jenni@ohsomodern.org>
2021-12-27 11:52:09 +0200

    Create .eslintrc.js

---
+++ .eslintrc.js
@@ -0,0 +1,25 @@
+module.exports = {
+  root: true,
+  env: {
+    node: true,
+  },
+  extends: ['plugin:vue/vue3-essential', 'eslint:recommended', 'plugin:prettier/recommended'],
+  parserOptions: {
+    parser: '@babel/eslint-parser',
+  },
+  rules: {
+    'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    'no-unused-vars': process.env.NODE_ENV === 'production' ? 'error' : 'warn',
+    'no-unreachable': process.env.NODE_ENV === 'production' ? 'error' : 'warn',
+    'no-undef': process.env.NODE_ENV === 'production' ? 'error' : 'warn',
+    'max-len': ['error', { code: 120 }],
+    'vue/max-len': [
+      'error',
+      {
+        code: 120,
+        template: 150,
+      },
+    ],
+  },
+};
