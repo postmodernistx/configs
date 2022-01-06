038a2aba36e2329144f05cc4c626f6701197899d
Jenni <jenni@ohsomodern.org>
2022-01-06 09:56:34 +0200

    Update .eslintrc.js

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,6 +1,8 @@
 module.exports = {
   root: true,
   env: {
+    browser: true,
+    es6: true,
     node: true,
   },
   extends: ['plugin:vue/vue3-essential', 'eslint:recommended', 'plugin:prettier/recommended'],
