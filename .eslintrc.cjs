8bbb73206f14b242ce3e6c4c31f4aa142a64b25d
Jenni <jenni@ohsomodern.org>
2022-11-27 15:18:44 +0200

    Update .eslintrc.cjs

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,24 +1,37 @@
 module.exports = {
-    "env": {
-        "browser": true,
-        "es2021": true
+  env: {
+    browser: true,
+    es2021: true
+  },
+  extends: 'standard-with-typescript',
+  overrides: [
+    {
+      files: ['*.ts', '*.tsx'],
+      extends: [
+        'plugin:@typescript-eslint/recommended',
+        'plugin:@typescript-eslint/recommended-requiring-type-checking',
+      ],
+      parserOptions: {
+        project: ['./tsconfig.json'],
+      },
     },
-    "extends": "airbnb-base",
-    "overrides": [
-    ],
-    "parserOptions": {
-        "ecmaVersion": "latest",
-        "sourceType": "module"
-    },
-    "rules": {
-        curly: ['error', 'all'],
-        indent: ['error', 2, { SwitchCase: 1 }],
-        'max-len': ['error', { code: 120 }],
-        'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
-        'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
-        'no-plusplus': ['error', { allowForLoopAfterthoughts: true }],
-        'no-unused-vars': process.env.NODE_ENV === 'production' ? 'error' : 'warn',
-        'no-unreachable': process.env.NODE_ENV === 'production' ? 'error' : 'warn',
-        'no-undef': process.env.NODE_ENV === 'production' ? 'error' : 'warn',
-    }
+  ],
+  parser: '@typescript-eslint/parser',
+  plugins: ['@typescript-eslint'],
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+    project: true,
+  },
+  rules: {
+    curly: ['error', 'all'],
+    indent: ['error', 2, { SwitchCase: 1 }],
+    'max-len': ['error', { code: 120 }],
+    'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    'no-plusplus': ['error', { allowForLoopAfterthoughts: true }],
+    'no-unused-vars': process.env.NODE_ENV === 'production' ? 'error' : 'warn',
+    'no-unreachable': process.env.NODE_ENV === 'production' ? 'error' : 'warn',
+    'no-undef': process.env.NODE_ENV === 'production' ? 'error' : 'warn',
+  }
 }
