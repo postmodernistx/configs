038baa3530f267c06772fba2dc206621c1160a85
Jenni <jenni@ohsomodern.org>
2023-08-01 11:06:54 +0300

    Update commitlint.config.js

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,17 +1,10 @@
 module.exports = {extends: ['gitmoji']};
 
 /*
-package.json dependencies:
+Install:
+pnpm add -D @commitlint/cli @commitlint/config-conventional commitizen commitlint commitlint-config-gitmoji cz-conventional-changelog husky
 
-"@commitlint/cli": "^17.6.5",
-"@commitlint/config-conventional": "^17.6.5",
-"commitizen": "^4.3.0",
-"commitlint": "^17.6.5",
-"commitlint-config-gitmoji": "^2.3.1",
-"cz-conventional-changelog": "^3.3.0",
-"husky": "^8.0.3",
-
-And config:
+In package.json config section:
 
 "config": {
   "commitizen": {
