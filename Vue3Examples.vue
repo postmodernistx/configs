0c42ec5fbc1dd5f029433be83855f7829d16a7d8
Jenni <jenni@ohsomodern.org>
2023-08-01 16:45:50 +0300

    Create Vue3Examples.vue

---
+++ Vue3Examples.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+// Typed props with default values
+interface Props {
+  type?: 'normal' | 'warning' | 'danger'; // Optional
+  display: 'block' | 'inlined'; // Required
+}
+
+const props = withDefaults(defineProps<Props>(), {
+  type: 'normal',
+  display: 'block',
+});
+</script>
