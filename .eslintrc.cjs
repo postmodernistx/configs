684398446c0f46ca7167f30ce50b290df61a9e53
Jenni <jenni@ohsomodern.org>
2024-04-28 13:44:28 +0300

    Update .eslintrc.cjs

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -29,15 +29,15 @@ module.exports = {
   rules: {
     '@typescript-eslint/comma-dangle': 'off',
     '@typescript-eslint/semi': 'off',
-    'arrow-spacing': ['error', { 'before': true, 'after': true }],
+    'arrow-spacing': ['error', { before: true, after: true }],
     'block-spacing': 'error',
-    'brace-style': ['error', '1tbs', { 'allowSingleLine': false}],
+    'brace-style': ['error', '1tbs', { allowSingleLine: false }],
     'comma-dangle': ['error', 'always-multiline'],
-    'curly': ['error', 'all'],
+    curly: ['error', 'all'],
     'import/no-absolute-path': 'off',
-    'indent': ['error', 2, { SwitchCase: 1 }],
-    'key-spacing': ['error', { 'afterColon': true, 'beforeColon': false }],
-    'keyword-spacing': ['error', { 'before': true }],
+    indent: ['error', 2, { SwitchCase: 1 }],
+    'key-spacing': ['error', { afterColon: true, beforeColon: false }],
+    'keyword-spacing': ['error', { before: true }],
     'max-len': ['error', { code: 120 }],
     'no-console': process.env.PROD ? 'warn' : 'off',
     'no-debugger': process.env.PROD ? 'warn' : 'off',
@@ -46,9 +46,9 @@ module.exports = {
     'no-unreachable': process.env.PROD ? 'error' : 'warn',
     'no-unused-vars': process.env.PROD ? 'error' : 'warn',
     'object-curly-spacing': ['error', 'always'],
-    'quotes': ['error', 'single'],
-    'semi': ['error', 'always'],
+    quotes: ['error', 'single'],
+    semi: ['error', 'always'],
     'space-before-blocks': ['error', 'always'],
-    'space-infix-ops': ['error', { 'int32Hint': false }]
-  }
-}
+    'space-infix-ops': ['error', { int32Hint: false }],
+  },
+};
